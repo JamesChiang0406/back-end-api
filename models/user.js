332e8aae9a01bb8403b025680eb9966ca731bc56
James_C <a9356070@gmail.com>
2021-07-06 15:45:07 +0800

    users-seeds-file done.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    account: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    cover: DataTypes.STRING,
+    introduction: DataTypes.STRING,
+    role: DataTypes.STRING
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
   };
   return User;
 };
